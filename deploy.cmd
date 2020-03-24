a8a34911022f7a3402ac821b48549314992682e8
taozeran <841070682@qq.com>
2020-03-24 13:58:28 +0800

    add travis config and deploy script

---
+++ deploy.cmd
@@ -0,0 +1,4 @@
+
+scp ./target/demo-0.0.1-SNAPSHOT.jar "root@%SERVER_IP%:/home/app.jar"
+ssh "root@%SERVER_IP%" "kill -9 `pidof java`"
+ssh "root@%SERVER_IP%" "java -jar /home/app.jar &"
--- src/main/java/com/example/demo/api/AuthController.java
+++ src/main/java/com/example/demo/api/AuthController.java
@@ -1,29 +1,29 @@
-package com.example.demo.api;/**
- * 　　　　　　　 ┏┓       ┏┓+ +
- * 　　　　　　　┏┛┻━━━━━━━┛┻┓ + +
- * 　　　　　　　┃　　　　　　 ┃
- * 　　　　　　　┃　　　━　　　┃ ++ + + +
- * 　　　　　　 █████━█████  ┃+
- * 　　　　　　　┃　　　　　　 ┃ +
- * 　　　　　　　┃　　　┻　　　┃
- * 　　　　　　　┃　　　　　　 ┃ + +
- * 　　　　　　　┗━━┓　　　 ┏━┛
- * ┃　　  ┃
- * 　　　　　　　　　┃　　  ┃ + + + +
- * 　　　　　　　　　┃　　　┃　Code is far away from     bug with the animal protecting
- * 　　　　　　　　　┃　　　┃ + 　　　　         神兽保佑,代码无bug
- * 　　　　　　　　　┃　　　┃
- * 　　　　　　　　　┃　　　┃　　+
- * 　　　　　　　　　┃　 　 ┗━━━┓ + +
- * 　　　　　　　　　┃ 　　　　　┣┓
- * 　　　　　　　　　┃ 　　　　　┏┛
- * 　　　　　　　　　┗┓┓┏━━━┳┓┏┛ + + + +
- * 　　　　　　　　　 ┃┫┫　 ┃┫┫
- * 　　　　　　　　　 ┗┻┛　 ┗┻┛+ + + +
- */
+package com.example.demo.api;
 
-/**
- * Created by taozeran on 2020/3/23/18:59
- */
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping("/auth")
+@Slf4j
 public class AuthController {
+    @ResponseBody
+    @RequestMapping(value = "/test", method = RequestMethod.GET, produces = { "application/json;charset=utf-8" })
+    @ApiOperation(value = "测试微信公众号的接口配置信息", notes = "接口配置信息", httpMethod = "GET")
+    public String getWxUserInfo(HttpServletRequest request,
+                                @ApiParam(value = "微信求的 echostr") @RequestParam(required = true) String echostr
+    ) {
+        try {
+            //只需要把微信请求的 echostr, 返回给微信就可以
+            log.info("测试来过===================" + echostr);
+            return echostr;
+        } catch (Exception e) {
+            log.info("测试微信公众号的接口配置信息发生异常：", e);
+            return "系统异常！";
+        }
+    }
 }
